a7f71ba180328fabe956a4757cd33edf25245357
hello <hello@world.com>
2023-06-11 01:10:26 +0800

    2

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -37,7 +37,11 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
             return $this->addColumn('enum', $column, compact('length', 'allowed'))->comment($enumClass::comment($comment));
         });
 
-        Blueprint::macro('xPercent', function (string $column, $total = 5, $places = 2, $unsigned = false) {
+        Blueprint::macro('xPercent', function (string $column, $total = 3, $places = 2, $unsigned = false) {
+            return $this->addColumn('float', $column, compact('total', 'places', 'unsigned'));
+        });
+
+        Blueprint::macro('xMoney', function (string $column, $total = 10, $places = 2, $unsigned = false) {
             return $this->addColumn('float', $column, compact('total', 'places', 'unsigned'));
         });
     }
